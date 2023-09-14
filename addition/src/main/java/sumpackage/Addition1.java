bf0a520109d1bad30ec0ab40565fbc2a3281050b
HP <HP@DESKTOP-72GO13P>
2023-09-14 14:41:32 +0530

    Addition program

---
+++ addition/src/main/java/sumpackage/Addition1.java
@@ -0,0 +1,13 @@
+package sumpackage;
+
+public class Addition1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5;
+int b=5;
+int c=a+b;
+System.out.println(c);
+	}
+
+}
